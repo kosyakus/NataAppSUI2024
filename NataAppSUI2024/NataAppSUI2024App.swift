b15c81d3f3a6b5843f6bfebfe4e0f5501fc8b4e4
Natali <ns@numbuster.com>
2024-09-05 20:07:48 +0300

    Initial Commit

---
+++ NataAppSUI2024/NataAppSUI2024App.swift
@@ -0,0 +1,17 @@
+//
+//  NataAppSUI2024App.swift
+//  NataAppSUI2024
+//
+//  Created by Natalia Sinitsyna on 05.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct NataAppSUI2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
